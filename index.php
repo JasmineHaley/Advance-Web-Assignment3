922a95e4d46e8daa571dec8e0683e895cf261f17
JasmineHaley <47067968+JasmineHaley@users.noreply.github.com>
2020-12-05 13:29:38 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+//use Apps\handlers\IndexController;
+use Quwius\Framework\FrontController;
+require '/Applications/XAMPP/xamppfiles/400001020/assignment3_00/config.php'; 
+require ROOT_DIR.'/autoloader.php';
+ 
+FrontController :: run();
+ ?>
